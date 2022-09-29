280bfa3f163dd68809962700a425cb9b9fb5fbc2
ykukmme <rdijk021457@gmail.com>
2022-09-29 20:17:02 +0900

    bmi

--- aoppart2chapter01/app/src/main/java/fastcampus/aop/part2/chapter01/MainActivity.kt
+++ aoppart2chapter01/app/src/main/java/fastcampus/aop/part2/chapter01/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //xml과 연결하기
         val heightEditText: EditText = findViewById(R.id.heightEditText)
         val weightEditText = findViewById<EditText>(R.id.weightEditText)
 
@@ -22,11 +23,13 @@ class MainActivity : AppCompatActivity() {
 
             if (heightEditText.text.isEmpty() || weightEditText.text.isEmpty()){
                 Toast.makeText(this, "빈 값이 있습니다.", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
+                return@setOnClickListener //클릭 리스너 처음으로 돌아가기
             }
+            //입력받은 값을 Int로 변환하여 변수에 저장
             val height: Int = heightEditText.text.toString().toInt()
             val weight: Int = weightEditText.text.toString().toInt()
 
+            //인텐트 만들어서 값들을 보내고 화면 전환
             val intent = Intent(this, ResultActivity::class.java)
 
             intent.putExtra("height", height)
--- aoppart2chapter01/app/src/main/java/fastcampus/aop/part2/chapter01/ResultActivity.kt
+++ aoppart2chapter01/app/src/main/java/fastcampus/aop/part2/chapter01/ResultActivity.kt
@@ -11,10 +11,13 @@ class ResultActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
+        //메인 액티비티에서 값 받아오기
         val height = intent.getIntExtra("height", 0)
         val weight = intent.getIntExtra("weight", 0)
 
         val bmi = weight / (height / 100.0).pow(2.0)
+
+        //bmi 값에따라 표시할 상태 정리
         val resultText = when{
             bmi >= 35.0 -> "고도 비만"
             bmi >= 30.0 -> "중정도 비만"
@@ -23,9 +26,11 @@ class ResultActivity: AppCompatActivity() {
             bmi >= 18.5 -> "정상체중"
             else -> "저체중"
         }
+        //xml과 연결
         val resultValueTextView = findViewById<TextView>(R.id.bmiReultTextView)
         val resultStringTextView = findViewById<TextView>(R.id.resultTextView)
 
+        //텍스트로 결과 표시하기
         resultValueTextView.text = bmi.toString()
         resultStringTextView.text = resultText
     }
